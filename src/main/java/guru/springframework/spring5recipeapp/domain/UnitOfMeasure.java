58efd43febc515f5dddb4b734c3bdeb7755d0f73
Andrei Soloviev <hedg.r52@gmail.com>
2021-11-16 17:45:19 +0300

    completing assignment

--- src/main/java/guru/springframework/spring5recipeapp/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/UnitOfMeasure.java
@@ -1,32 +1,22 @@
 package guru.springframework.spring5recipeapp.domain;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import java.math.BigDecimal;
 
 /**
  * Created by Andrei Soloviev (hedg.r52@gmail.com).
  * date: 16.11.2021
  */
 @Entity
-public class Ingredient {
+public class UnitOfMeasure {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String description;
-    private BigDecimal amount;
-
-    @OneToOne(fetch = FetchType.EAGER)
-    private UnitOfMeasure uom;
-
-    @ManyToOne
-    private Recipe recipe;
 
     public Long getId() {
         return id;
@@ -42,21 +32,5 @@ public class Ingredient {
 
     public void setDescription(String description) {
         this.description = description;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
     }
 }
