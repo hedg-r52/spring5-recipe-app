31abd2f84bfbdcfda825f765a3e89f393de1c1f8
Andrei Soloviev <hedg.r52@gmail.com>
2021-11-16 17:49:52 +0300

    adding enum difficulty

---
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Difficulty.java
@@ -0,0 +1,9 @@
+package guru.springframework.spring5recipeapp.domain;
+
+/**
+ * Created by Andrei Soloviev (hedg.r52@gmail.com).
+ * date: 16.11.2021
+ */
+public enum Difficulty {
+    EASY, MODERATE, KIND_OF_HARD, HARD;
+}
