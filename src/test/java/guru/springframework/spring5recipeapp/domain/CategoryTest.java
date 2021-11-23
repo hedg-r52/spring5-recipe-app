54fdeec563b6811cdb08b11cd06cba65da711f04
Andrei Soloviev <hedg.r52@gmail.com>
2021-11-23 11:17:29 +0300

    Adding JUnit5 testing

---
+++ src/test/java/guru/springframework/spring5recipeapp/domain/CategoryTest.java
@@ -0,0 +1,37 @@
+package guru.springframework.spring5recipeapp.domain;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Andrei Soloviev (hedg.r52@gmail.com).
+ * date: 20.11.2021
+ */
+class CategoryTest {
+
+    Category category;
+
+    @BeforeEach
+    public void setUp() {
+        category = new Category();
+    }
+
+    @Test
+    void getId() {
+        Long idValue = 4L;
+        category.setId(idValue);
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    void getRecipes() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+}
