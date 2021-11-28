0ad53b3604dc6bb5aa7c882abd4dbe4c229e0ad9
Andrei Soloviev <hedg.r52@gmail.com>
2021-11-28 22:35:29 +0300

    delete an ingredient

--- src/test/java/guru/springframework/spring5recipeapp/controllers/IngredientControllerTest.java
+++ src/test/java/guru/springframework/spring5recipeapp/controllers/IngredientControllerTest.java
@@ -125,4 +125,17 @@ class IngredientControllerTest {
         verify(recipeService).findCommandById(anyLong());
 
     }
+
+    @Test
+    public void testDeleteIngredient() throws Exception {
+
+        //then
+        mockMvc.perform(get("/recipe/2/ingredient/3/delete")
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/recipe/2/ingredients"));
+
+        verify(ingredientService).deleteById(anyLong(), anyLong());
+
+    }
 }
--- src/test/java/guru/springframework/spring5recipeapp/services/IngredientServiceImplTest.java
+++ src/test/java/guru/springframework/spring5recipeapp/services/IngredientServiceImplTest.java
@@ -103,4 +103,23 @@ class IngredientServiceImplTest {
         verify(recipeRepository).findById(anyLong());
         verify(recipeRepository).save(any(Recipe.class));
     }
+
+    @Test
+    public void testDeleteById() throws Exception {
+        //given
+        Recipe recipe = new Recipe();
+        Ingredient ingredient = new Ingredient();
+        ingredient.setId(3L);
+        recipe.addIngredient(ingredient);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        //when
+        ingredientService.deleteById(1L, 3L);
+
+        //then
+        verify(recipeRepository).findById(anyLong());
+        verify(recipeRepository).save(any(Recipe.class));
+    }
 }
