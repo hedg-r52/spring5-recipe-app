6326ac94fd9e21d65f56821c9888348a0c6905c9
Andrei Soloviev <hedg.r52@gmail.com>
2021-12-10 08:15:15 +0300

    Exception handler

--- src/test/java/guru/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
@@ -104,6 +104,7 @@ class RecipeControllerTest {
     public void testGetRecipeNotFound() throws Exception {
         when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
         mockMvc.perform(get("/recipe/1/show"))
-                .andExpect(status().isNotFound());
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("404error"));
     }
 }
